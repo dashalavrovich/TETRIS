8e2a7fa35b4a8efd1dc4ffb0df73b20d9aca121c
Dasha <dashalave17@gmail.com>
2024-04-04 00:11:39 +0300

    edit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+import Game from './modules/game.js';
+import View from './modules/view.js';
+import Controller from './modules/controller.js';
+import Sound from './modules/sound.js';
+
+const root = document.getElementById('root');
+const options = {
+  width: 340,
+  height: 420,
+  rows: 20,
+  colums: 10,
+};
+
+const sound = new Sound(root);
+const game = new Game(sound);
+const view = new View(root, options);
+const controller = new Controller(game, view, sound);
+
+
+window.game = game;
+window.view = view;
+window.controller = controller;
+window.sound = sound;
